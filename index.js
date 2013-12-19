b605a15c6daba84948faed2f5f66e8f76e4e7b0b
mfstephens <mfstep@umich.edu>
2013-12-19 01:45:43 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// Import all modules
+var express = require('express'),
+	path = require('path'),
+	app = express(),
+	battle = require('./battlegame');
+
+// Create a simple Express application
+app.configure(function() {
+    // Turn down the logging activity
+    app.use(express.logger('dev'));
+
+    // Serve static html, js, css, and image files from the 'public' directory
+    app.use(express.static(path.join(__dirname,'public')));
+});
+
+// Create a Node.js based http server on port 8080
+var server = require('http').createServer(app).listen(8080);
+
+// Create a Socket.IO server and attach it to the http server
+var io = require('socket.io').listen(server);
+
+// Reduce the logging output of Socket.IO
+io.set('log level', 1);
+
+// Listen for Socket.IO Connections. Once connected, start the game logic.
+io.sockets.on('connection', function (socket) {
+    //console.log('client connected');
+    battle.initGame(io, socket);
+});
+
+
