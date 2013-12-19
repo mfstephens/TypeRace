ca68a616f8f4f1ff2579782e5410b5d6f4fed036
mfstephens <mfstep@umich.edu>
2013-12-19 15:26:36 -0500

    Practice stuff working

--- battlegame.js
+++ battlegame.js
@@ -1,3 +1,7 @@
+/*
+ * Code for all server logic contained in battlegame.js
+ */
+
 var io;
 var gameSocket;
 
@@ -6,36 +10,22 @@ var gameSocket;
  *
  * @param sio The Socket.IO library
  * @param socket The socket object for the connected client.
- *
- * Exports makes initGame accessible to index.js!
  */
 exports.initGame = function(sio, socket){
     io = sio;
     gameSocket = socket;
     gameSocket.emit('connected', { message: "You are connected!" });
 
-    // Game Room Events
-    gameSocket.on('hostCreateNewGame', hostCreateNewGame);
-    gameSocket.on('hostRoomFull', hostPrepareGame);
-    gameSocket.on('hostCountdownFinished', hostStartGame);
-    gameSocket.on('hostNextRound', hostNextRound);
-
     // Player Events
+    gameSocket.on('playerCreateNewGame', playerCreateNewGame);
     gameSocket.on('playerJoinGame', playerJoinGame);
-    gameSocket.on('playerAnswer', playerAnswer);
-    gameSocket.on('playerRestart', playerRestart);
-}
+    gameSocket.on('playerSubmitText', playerSubmitText);
+};
 
-/* *******************************
-   *                             *
-   *       HOST FUNCTIONS        *
-   *                             *
-   ******************************* */
-
-/**
- * The 'START' button was clicked and 'hostCreateNewGame' event occurred.
+/*
+ * A player is creating a new game with a unique room ID
  */
-function hostCreateNewGame() {
+function playerCreateNewGame() {
     // Create a unique Socket.IO Room
     var thisGameId = ( Math.random() * 100000 ) | 0;
 
@@ -46,59 +36,11 @@ function hostCreateNewGame() {
     this.join(thisGameId.toString());
 };
 
-/*
- * Two players have joined. Alert the host!
- * @param gameId The game ID / room ID
- */
-function hostPrepareGame(gameId) {
-    var sock = this;
-    var data = {
-        mySocketId : sock.id,
-        gameId : gameId
-    };
-    //console.log("All Players Present. Preparing game...");
-    io.sockets.in(data.gameId).emit('beginNewGame', data);
-}
-
-/*
- * The Countdown has finished, and the game begins!
- * @param gameId The game ID / room ID
- */
-function hostStartGame(gameId) {
-    console.log('Game Started.');
-    sendWord(0,gameId);
-};
-
-/**
- * A player answered correctly. Time for the next word.
- * @param data Sent from the client. Contains the current round and gameId (room)
- */
-function hostNextRound(data) {
-    if(data.round < wordPool.length ){
-        // Send a new set of words back to the host and players.
-        sendWord(data.round, data.gameId);
-    } else {
-        // If the current round exceeds the number of words, send the 'gameOver' event.
-        io.sockets.in(data.gameId).emit('gameOver',data);
-    }
-}
-/* *****************************
-   *                           *
-   *     PLAYER FUNCTIONS      *
-   *                           *
-   ***************************** */
-
-/**
- * A player clicked the 'START GAME' button.
- * Attempt to connect them to the room that matches
- * the gameId entered by the player.
- * @param data Contains data entered via player's input - playerName and gameId.
- */
 function playerJoinGame(data) {
-    //console.log('Player ' + data.playerName + 'attempting to join game: ' + data.gameId );
 
     // A reference to the player's Socket.IO socket object
     var sock = this;
+
 
     // Look up the room ID in the Socket.IO manager object.
     var room = gameSocket.manager.rooms["/" + data.gameId];
@@ -111,7 +53,7 @@ function playerJoinGame(data) {
         // Join the room
         sock.join(data.gameId);
 
-        //console.log('Player ' + data.playerName + ' joining game: ' + data.gameId );
+        console.log('Player ' + data.playerName + ' joining game: ' + data.gameId );
 
         // Emit an event notifying the clients that the player has joined the room.
         io.sockets.in(data.gameId).emit('playerJoinedRoom', data);
@@ -120,162 +62,8 @@ function playerJoinGame(data) {
         // Otherwise, send an error message back to the player.
         this.emit('error',{message: "This room does not exist."} );
     }
+};
+
+function playerSubmitText(data) {
+    io.sockets.in(data.gameId).emit('displayPlayerText', data);
 }
-
-/**
- * A player has tapped a word in the word list.
- * @param data gameId
- */
-function playerAnswer(data) {
-    // console.log('Player ID: ' + data.playerId + ' answered a question with: ' + data.answer);
-
-    // The player's answer is attached to the data object.  \
-    // Emit an event with the answer so it can be checked by the 'Host'
-    io.sockets.in(data.gameId).emit('hostCheckAnswer', data);
-}
-
-/**
- * The game is over, and a player has clicked a button to restart the game.
- * @param data
- */
-function playerRestart(data) {
-    // console.log('Player: ' + data.playerName + ' ready for new game.');
-
-    // Emit the player's data back to the clients in the game room.
-    data.playerId = this.id;
-    io.sockets.in(data.gameId).emit('playerJoinedRoom',data);
-}
-
-/* *************************
-   *                       *
-   *      GAME LOGIC       *
-   *                       *
-   ************************* */
-
-/**
- * Get a word for the host, and a list of words for the player.
- *
- * @param wordPoolIndex
- * @param gameId The room identifier
- */
-function sendWord(wordPoolIndex, gameId) {
-    var data = getWordData(wordPoolIndex);
-    io.sockets.in(data.gameId).emit('newWordData', data);
-}
-
-/**
- * This function does all the work of getting a new words from the pile
- * and organizing the data to be sent back to the clients.
- *
- * @param i The index of the wordPool.
- * @returns {{round: *, word: *, answer: *, list: Array}}
- */
-function getWordData(i){
-    // Randomize the order of the available words.
-    // The first element in the randomized array will be displayed on the host screen.
-    // The second element will be hidden in a list of decoys as the correct answer
-    var words = shuffle(wordPool[i].words);
-
-    // Randomize the order of the decoy words and choose the first 5
-    var decoys = shuffle(wordPool[i].decoys).slice(0,5);
-
-    // Pick a random spot in the decoy list to put the correct answer
-    var rnd = Math.floor(Math.random() * 5);
-    decoys.splice(rnd, 0, words[1]);
-
-    // Package the words into a single object.
-    var wordData = {
-        round: i,
-        word : words[0],   // Displayed Word
-        answer : words[1], // Correct Answer
-        list : decoys      // Word list for player (decoys and answer)
-    };
-
-    return wordData;
-}
-
-/*
- * Javascript implementation of Fisher-Yates shuffle algorithm
- * http://stackoverflow.com/questions/2450954/how-to-randomize-a-javascript-array
- */
-function shuffle(array) {
-    var currentIndex = array.length;
-    var temporaryValue;
-    var randomIndex;
-
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-/**
- * Each element in the array provides data for a single round in the game.
- *
- * In each round, two random "words" are chosen as the host word and the correct answer.
- * Five random "decoys" are chosen to make up the list displayed to the player.
- * The correct answer is randomly inserted into the list of chosen decoys.
- *
- * @type {Array}
- */
-var wordPool = [
-    {
-        "words"  : [ "sale","seal","ales","leas" ],
-        "decoys" : [ "lead","lamp","seed","eels","lean","cels","lyse","sloe","tels","self" ]
-    },
-
-    {
-        "words"  : [ "item","time","mite","emit" ],
-        "decoys" : [ "neat","team","omit","tame","mate","idem","mile","lime","tire","exit" ]
-    },
-
-    {
-        "words"  : [ "spat","past","pats","taps" ],
-        "decoys" : [ "pots","laps","step","lets","pint","atop","tapa","rapt","swap","yaps" ]
-    },
-
-    {
-        "words"  : [ "nest","sent","nets","tens" ],
-        "decoys" : [ "tend","went","lent","teen","neat","ante","tone","newt","vent","elan" ]
-    },
-
-    {
-        "words"  : [ "pale","leap","plea","peal" ],
-        "decoys" : [ "sale","pail","play","lips","slip","pile","pleb","pled","help","lope" ]
-    },
-
-    {
-        "words"  : [ "races","cares","scare","acres" ],
-        "decoys" : [ "crass","scary","seeds","score","screw","cager","clear","recap","trace","cadre" ]
-    },
-
-    {
-        "words"  : [ "bowel","elbow","below","beowl" ],
-        "decoys" : [ "bowed","bower","robed","probe","roble","bowls","blows","brawl","bylaw","ebola" ]
-    },
-
-    {
-        "words"  : [ "dates","stead","sated","adset" ],
-        "decoys" : [ "seats","diety","seeds","today","sited","dotes","tides","duets","deist","diets" ]
-    },
-
-    {
-        "words"  : [ "spear","parse","reaps","pares" ],
-        "decoys" : [ "ramps","tarps","strep","spore","repos","peris","strap","perms","ropes","super" ]
-    },
-
-    {
-        "words"  : [ "stone","tones","steno","onset" ],
-        "decoys" : [ "snout","tongs","stent","tense","terns","santo","stony","toons","snort","stint" ]
-    }
-]
---
+++ public/app.js
@@ -0,0 +1,131 @@
+;
+jQuery(function($){    
+    'use strict';
+
+    var IO = {
+        /**
+         * This is called when the page is displayed. It connects the Socket.IO client
+         * to the Socket.IO server
+         */
+        init: function() {
+            IO.socket = io.connect();
+            IO.bindEvents();
+        },
+
+        /**
+         * While connected, Socket.IO will listen to the following events emitted
+         * by the Socket.IO server, then run the appropriate function.
+         */
+        bindEvents : function() {
+            IO.socket.on('connected', IO.onConnected );
+            IO.socket.on('newGameCreated', IO.onNewGameCreated );
+            IO.socket.on('displayPlayerText', IO.onDisplayPlayerText);
+        },
+
+        /**
+         * The client is successfully connected!
+         */
+        onConnected : function() {
+            // Cache a copy of the client's socket.IO session ID on the App
+            App.mySocketId = IO.socket.socket.sessionid;
+            console.log("Client connected!");
+        },
+
+        /**
+         * A new game has been created and a random game ID has been generated.
+         * @param data {{ gameId: int, mySocketId: * }}
+         */
+        onNewGameCreated : function(data) {
+            App.gameInit(data);
+        },
+
+        onDisplayPlayerText: function(data) {
+            App.displayPlayerText(data);
+        }
+    };
+
+    var App = {
+        /**
+         * Keep track of the gameId, which is identical to the ID
+         * of the Socket.IO Room used for the players and host to communicate
+         *
+         */
+        gameId: 0,
+
+        /**
+         * This is used to differentiate between 'Host' and 'Player' browsers.
+         */
+        myRole: '',   // 'Player' or 'Host'
+
+        /**
+         * The Socket.IO socket object identifier. This is unique for
+         * each player and host. It is generated when the browser initially
+         * connects to the server when the page loads for the first time.
+         */
+        mySocketId: '',
+
+        /* *************************************
+         *                Setup                *
+         * *********************************** */
+
+        /**
+         * This runs when the page initially loads.
+         */
+        init: function () {
+            App.bindEvents();
+        },
+
+        /**
+         * Create some click handlers for the various buttons that appear on-screen.
+         */
+        bindEvents: function () {
+            $('#btnCreateGame').click(App.onCreateClick);
+            $('#btnJoinGame').click(App.onPlayerJoinClick);
+            $('#btnSubmitText').click(App.onPlayerSubmitText);
+        },
+
+        /**
+         * Handler for the "Start" button on the Title Screen.
+         */
+        onCreateClick: function () {
+            // console.log('Clicked "Create A Game"');
+            IO.socket.emit('playerCreateNewGame');
+        },
+
+        onPlayerJoinClick: function () {
+            var data = {
+                gameId: +($("#inptJoinGame").val()),
+                playerName: $("#inptPlayerName").val()
+            }
+            App.gameId = data.gameId;
+            IO.socket.emit('playerJoinGame', data);
+            console.log('Clicked "Join A Game"');
+        },
+
+        onPlayerSubmitText: function() {
+            var data = {
+                text: $("#chatInputWindow").val(),
+                gameId: App.gameId
+            }
+            IO.socket.emit('playerSubmitText', data);
+        },
+
+        displayPlayerText: function(data) {
+            console.log(data.text);
+            $("#chatOutputWindow").text(data.text);
+        },
+
+        gameInit: function (data) {
+            App.gameId = data.gameId;
+            App.mySocketId = data.mySocketId;
+            App.myRole = 'Host';
+            App.numPlayersInRoom = 0;
+
+            console.log("gameID: " + App.gameId);
+        }
+    };
+
+    IO.init();
+    App.init();
+
+}($));
